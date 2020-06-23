98e101fbf45f987d14091514ffe456b0c0437ec3
bajman <mail@yaleleber.com>
2020-06-23 06:57:10 -0400

    Update stackdash_menu.py

--- stackdash_menu.py
+++ stackdash_menu.py
@@ -210,7 +210,7 @@ Enter "yes" or "no" [or "m" to return to main menu; enter "q" to quit]:  """)
 
 def devops_traefik():
     #read input file
-    fin = open("./stacks/devops/traefik/.env", "w")
+    fin = open("./stacks/devops/traefik/.env", "wr")
     #read file contents to string
     data = fin.read()
     #add user input
