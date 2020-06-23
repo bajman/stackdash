0230f5e2c84350bf7f0d5ec896573bbf7eb5f330
bajman <mail@yaleleber.com>
2020-06-23 08:10:02 -0400

    Update stackdash_menu.py

--- stackdash_menu.py
+++ stackdash_menu.py
@@ -262,12 +262,12 @@ def devops_traefik():
     user_c_email = input("[Cloudflare – 1/3] \n Please enter your Cloudflare Email Address, [Email address for Cloudflare account, located at https://dash.cloudflare.com, e.g., mail@example.com]:  ")
     
     new_lines = []
-    with open(devops_env, 'w') as f:
+    with open('devops_env,' 'r+') as f:
         for line in f:
             if 'CF_API_EMAIL=C_EMAIL' in line:
                 new_lines.append(line.replace('C_EMAIL', 'user_c_email'))
 
-    with open('devops_env', 'w') as f:
+    with open('devops_env', 'r+') as f:
         f.write('\n'.join(new_lines))
 
     devops_env.close()
