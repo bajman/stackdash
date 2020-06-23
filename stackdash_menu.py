562c69f32c434fb3928cb66ce3866127bb1512b0
bajman <mail@yaleleber.com>
2020-06-23 09:08:31 -0400

    Update stackdash_menu.py

--- stackdash_menu.py
+++ stackdash_menu.py
@@ -256,8 +256,8 @@ def devops_traefik():
     traefik_env_copy = shutil.copytree('./stacks/devops/traefik/', '/opt/stack_dash/devops/traefik/', dirs_exist_ok=True)
     print ("*** Copied Traefik tree to /opt/stack_dash/devops/traefik. ***\n")
 
-    C_EMAIL = input("[Cloudflare: 1/3] \n Please enter your Cloudflare Email Address, [Email address for Cloudflare account, located at https://dash.cloudflare.com, e.g., mail@example.com]: ")
-    C_EMAIL_ENV = subprocess.run("sudo export CF_API_EMAIL='C_EMAIL'", shell=True)
-    print ("*** Added Cloudflare email address to environment variables ***")
-
+    c_email = input("[Cloudflare: 1/3] \n Please enter your Cloudflare Email Address, [Email address for Cloudflare account, located at https://dash.cloudflare.com, e.g., mail@example.com]:  ")
+    os.environ['CF_API_EMAIL'] = 'c_email'
+    print("CF_API_EMAIL:", os.environ['CF_API_EMAIL'])
+    
 main()
