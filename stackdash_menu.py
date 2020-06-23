c3d5d188d695827b6a343bfc506232b97e2b4512
bajman <mail@yaleleber.com>
2020-06-23 05:08:59 -0400

    Update stackdash_menu.py

--- stackdash_menu.py
+++ stackdash_menu.py
@@ -295,7 +295,7 @@ def devops_traefik():
     guacamole_dir_permissions = subprocess.run("sudo chmod 777 -R /opt/stack_dash/devops/apache_guacamole", capture_output=True, shell=True)
     print ("*** Corrected Portainer folder permissions. ***\n")
     
-f1 = open('./stackdash/stacks/devops/traefik/.env', 'a+')
+f1 = open('/stackdash/stacks/devops/traefik/.env', 'a+')
 for line in f1:
     f1.write(line.replace('$C_EMAIL', 'input("[Cloudflare: 1/3] \n Please enter your Cloudflare Email Address, [Email address for Cloudflare account, located at https://dash.cloudflare.com, e.g., mail@example.com]:  "'))
 f1.close()
