723232d93ecc03ef7b540d1a940a87741b17240d
bajman <mail@yaleleber.com>
2020-06-23 05:16:02 -0400

    Update stackdash_menu.py

--- stackdash_menu.py
+++ stackdash_menu.py
@@ -19,7 +19,7 @@ def main():
 ╚════██║   ██║   ██╔══██║██║     ██╔═██╗     ██║  ██║██╔══██║╚════██║██╔══██║
 ███████║   ██║   ██║  ██║╚██████╗██║  ██╗    ██████╔╝██║  ██║███████║██║  ██║
 ╚══════╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝ \n
-StackDash is a python3 script, designed to automate Docker installation, container deployment, and subdomain hosting. \n
+StackDash is a python3 script, designed to automate Docker installation, container deployment, and subdomain hosting. \n\n
 ==============================================================================================================================================================
  ___  ___      ___       __   ___  __  
 |__  |__   /\   |  |  | |__) |__  /__` 
@@ -28,22 +28,22 @@ StackDash is a python3 script, designed to automate Docker installation, contain
 ==============================================================================================================================================================
 
 -- Docker Install Script
-	  Removes old versions of Dockers and the installs of the lastest version of Docker Engine on Ubunutu over HTTPS
+      Removes old versions of Dockers and the installs of the lastest version of Docker Engine on Ubunutu over HTTPS
       (supported versions: 20.04 LTS, 19.10, 18.04 LTS, and 16.04 LTS) \n
 
 -- Docker Containers/Stack Deployment Wizard
-	  Deploys stacks and individual Docker web app containers
+      Deploys stacks and individual Docker web app containers
       [Type [w] for a complete list of containers that can be depoyed with StackDash] \n
 
 -- Preconfigured Subdomains
-	  Dedicated container subdomains (e.g., portainer.example.com or bookstack.example.com) \n 
+      Dedicated container subdomains (e.g., portainer.example.com or bookstack.example.com) \n 
       [Type [s] for a  list of subdomains that must be added as CNAME records to your DNS service (see Documenation for additional information)] \n
 
 -- Automated Dashboard Deployment
       Deploys web dashboard/homepage for quick access to your Docker & server assets \n
 
 -- HTTPS/TLS/OAuth 2 Supported
-	  Traefik 2 routes external subdomain requests to the appropriate, internal server/container address.\n 
+      Traefik 2 routes external subdomain requests to the appropriate, internal server/container address.\n\n 
 
 ==============================================================================================================================================================
       ___ ___       __   __           __          __   __             
@@ -68,42 +68,37 @@ StackDash is a python3 script, designed to automate Docker installation, contain
   |                                     +----------------------------------------------------+   |
   |                                                       INTERNAL NETWORK                       |
   |                                                                                              |
-  +----------------------------------------------------------------------------------------------+
-
-==============================================================================================================================================================
+  +---------------
+  -------------------------------------------------------------------------------+
+\n\n
+==============================================================================
  __   ___  __        __          __   __       ___              ___  __   __  
 |  \ |__  |__) |    /  \ \ /    /  ` /  \ |\ |  |   /\  | |\ | |__  |__) /__` 
 |__/ |___ |    |___ \__/  |     \__, \__/ | \|  |  /~~\ | | \| |___ |  \ .__/ 
                                                                                                                                                           
-==============================================================================================================================================================
-                                                                                                                      
+==============================================================================                                                                                                           
 Type [w] for a complete list of web apps you can deploy with StackDash. 
-Type [c] to deploy individual containers. 
-
-==============================================================================================================================================================
+Type [c] to deploy individual containers.
+\n\n
+============================================================
  __   ___  __        __          __  ___       __        __  
 |  \ |__  |__) |    /  \ \ /    /__`  |   /\  /  ` |__/ /__` 
 |__/ |___ |    |___ \__/  |     .__/  |  /~~\ \__, |  \ .__/ 
                                                                                                                         
-==============================================================================================================================================================
-
+============================================================
 Type [u] for a complete list of stacks you can deploy with StackDash.
 Type [s] for a complete list of web app subdomain aliases used by StackDash. 
-Type [t] to deploy the Data, DevOps, or Media StackDash Stacks. \n\n\n
-
-==============================================================================================================================================================
+Type [t] to deploy the Data, DevOps, or Media StackDash Stacks. 
+\n\n
+==================
       __  ___  ___ 
 |\ | /  \  |  |__  
 | \| \__/  |  |___                  
                                                                                       
-==============================================================================================================================================================
-
----¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬--- 
-\n StackDash v. 0.1 is configured to support HTTPS & TLS only for domains managed by Cloudflare. Additional domain/hosting providers will be made available in future releases of StackDash. For a full list of planned DNS providers see: https://docs.traefik.io/https/acme/#providers. \n
----¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬--- 
-\n If you have domain name (e.g., example.com hosted by GoDaddy or Google Cloud) and would like to make Cloudflare your free, default DNS record manager: \n  
-You simply need to add two DNS records to your Cloudflare account. For more information on adding vendor-specific records to your free Cloudflare account, see: https://support.cloudflare.com/hc/en-us/articles/360020991331-Adding-vendor-specific-DNS-records-to-Cloudflare. Cloudflare can manage DNS records on domains hosted by Google Cloud, Amazon S3, Microsoft Azure, ClickFunnels, WPEngine, and Zoho. \n
----¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬---¬--- 
+==================
+This version of StackDash is configured to support only Cloudflare DNS-managed domains. Additional domain/hosting providers will be made available in future releases of StackDash. For a full list of planned DNS providers see: https://docs.traefik.io/https/acme/#providers.\n\n 
+If you have domain name (e.g., example.com hosted by GoDaddy or Google Cloud) and would like to make Cloudflare your free, default DNS record manager: \n  
+	You need to add two DNS domain records to your Cloudflare account. For more information on adding vendor-specific records to your 	  free Cloudflare account, see: https://support.cloudflare.com/hc/en-us/articles/360020991331-Adding-vendor-specific-DNS-records-           to-Cloudflare. Cloudflare can manage DNS records on domains hosted by Google Cloud, Amazon S3, Microsoft Azure, ClickFunnels,  	   WPEngine, and Zoho. \n\n
 \n If you do not have a domain, you can purchase one through Cloudflare (about $8.XX per year) at https://www.cloudflare.com/products/registrar/  
 
     Please enter your choice:  """)
